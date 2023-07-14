68cc6e24c625bc63d4d3a3fda304a79bb9930b59
AbhishekDacharla <abhi.dacharla0119@gmail.com>
2023-07-14 20:39:10 +0530

    FirstFile

---
+++ boolean.java
@@ -0,0 +1,23 @@
+package javapractice;
+
+public class BooleanValues1 {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=6;
+		if(a>b)
+		{
+			System.out.println("statement is true");	
+		}
+		else
+		{
+			System.out.println("statement is false");
+		}
+		
+	
+	
+
+
+}
+}
+
---
+++ method.java
@@ -0,0 +1,24 @@
+package javapractice;
+
+public class MethodDemo1 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Main Method of MethodDemo1");
+	}
+
+	void funA()
+	{
+		System.out.println("funA of MethodDemo1");
+	}
+	void funB()
+	{
+		System.out.println("funB of MethodDemo1");
+	}
+	static void funC()
+	{
+		System.out.println("funC of MethodDemo1");
+	}
+
+}
